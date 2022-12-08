740918db2513915c53456d45ba1db0107fcbedde
eduardo-rdguez <eduardoperaltardgz@gmail.com>
2022-12-08 14:07:44 -0600

    Build new spring project for microservice cart

---
+++ cart-service/src/main/kotlin/com/makingdevs/cartservice/CartServiceApplication.kt
@@ -0,0 +1,11 @@
+package com.makingdevs.cartservice
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class CartServiceApplication
+
+fun main(args: Array<String>) {
+	runApplication<CartServiceApplication>(*args)
+}
