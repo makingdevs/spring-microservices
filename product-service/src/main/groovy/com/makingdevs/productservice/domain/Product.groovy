272ef9f0d4fa39308904865fc61de470ef62316b
Gilberto Reyes <gilbott@MacBook-Pro-de-Gilberto.local>
2022-12-09 10:44:35 -0600

    add Table annotation to map PRODUCTS table

--- product-service/src/main/groovy/com/makingdevs/productservice/domain/Product.groovy
+++ product-service/src/main/groovy/com/makingdevs/productservice/domain/Product.groovy
@@ -1,11 +1,13 @@
 package com.makingdevs.productservice.domain
 
 import javax.persistence.Entity
+import javax.persistence.Table
 import javax.persistence.GeneratedValue
 import javax.persistence.GenerationType
 import javax.persistence.Id
 
 @Entity
+@Table(name = "PRODUCTS")
 public class Product {
 
   @Id
