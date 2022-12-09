8dc0ffe9c0ec64eea8cb5c23c3891498dbe93606
eduardo-rdguez <eduardoperaltardgz@gmail.com>
2022-12-08 18:18:42 -0600

    Remove lombok's unnecessary dependency on build.gradle

--- cart-service/build.gradle.kts
+++ cart-service/build.gradle.kts
@@ -29,7 +29,6 @@ dependencies {
 	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
-	compileOnly("org.projectlombok:lombok")
 	runtimeOnly("com.mysql:mysql-connector-j")
 	annotationProcessor("org.projectlombok:lombok")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
