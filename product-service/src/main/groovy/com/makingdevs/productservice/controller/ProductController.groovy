74321c452b862cc2ba3ab6717bf4de652b90f678
Gilberto Reyes <gilbott@MacBook-Pro-de-Gilberto.local>
2022-12-09 13:51:25 -0600

    add sendToCart route controller

--- product-service/src/main/groovy/com/makingdevs/productservice/controller/ProductController.groovy
+++ product-service/src/main/groovy/com/makingdevs/productservice/controller/ProductController.groovy
@@ -2,11 +2,15 @@ package com.makingdevs.productservice.controller
 
 import com.makingdevs.productservice.domain.Product
 import com.makingdevs.productservice.repository.ProductRepository
+import com.makingdevs.productservice.service.ProductService
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
@@ -15,6 +19,9 @@ class ProductController {
 
   @Autowired
   ProductRepository productRepository
+
+  @Autowired
+  ProductService productService
 
   @PostMapping("/addOne")
   Product addProduct(@RequestBody Product product) {
@@ -30,6 +37,14 @@ class ProductController {
   @GetMapping("/getAll")
   List<Product> getAllProduct () {
     return productRepository.findAll()
+  }
+
+  @PostMapping("/sendToCart/{productId}")
+  ResponseEntity<Product> sendToCart(@PathVariable Long productId) {
+    Product product = productService.sendToCart(productId)
+    product
+      ? new ResponseEntity<>(product, HttpStatus.OK)
+      : new ResponseEntity<>(HttpStatus.NOT_FOUND)
   }
 
 }
