3eb14c79fa890ab5982504790bf5a615c80801ae
eduardo-rdguez <eduardoperaltardgz@gmail.com>
2022-12-09 13:49:16 -0600

    Add and implement log4j-api-kotlin dependency

--- cart-service/build.gradle.kts
+++ cart-service/build.gradle.kts
@@ -1,11 +1,11 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-	id("org.springframework.boot") version "2.7.6"
-	id("io.spring.dependency-management") version "1.0.15.RELEASE"
-	kotlin("jvm") version "1.6.21"
-	kotlin("plugin.spring") version "1.6.21"
-	kotlin("plugin.jpa") version "1.6.21"
+  id("org.springframework.boot") version "2.7.6"
+  id("io.spring.dependency-management") version "1.0.15.RELEASE"
+  kotlin("jvm") version "1.6.21"
+  kotlin("plugin.spring") version "1.6.21"
+  kotlin("plugin.jpa") version "1.6.21"
 }
 
 group = "com.makingdevs"
@@ -13,35 +13,35 @@ version = "0.0.1-SNAPSHOT"
 java.sourceCompatibility = JavaVersion.VERSION_1_8
 
 configurations {
-	compileOnly {
-		extendsFrom(configurations.annotationProcessor.get())
-	}
+  compileOnly {
+    extendsFrom(configurations.annotationProcessor.get())
+  }
 }
 
 repositories {
-	mavenCentral()
+  mavenCentral()
 }
 
 dependencies {
-	implementation("org.springframework.boot:spring-boot-starter-amqp")
-	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
-	implementation("org.springframework.boot:spring-boot-starter-web")
-	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
-	implementation("org.jetbrains.kotlin:kotlin-reflect")
-	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
-	runtimeOnly("com.mysql:mysql-connector-j")
-	annotationProcessor("org.projectlombok:lombok")
-	testImplementation("org.springframework.boot:spring-boot-starter-test")
-	testImplementation("org.springframework.amqp:spring-rabbit-test")
+  implementation("org.springframework.boot:spring-boot-starter-amqp")
+  implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+  implementation("org.springframework.boot:spring-boot-starter-web")
+  implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
+  implementation("org.jetbrains.kotlin:kotlin-reflect")
+  implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+  implementation("org.apache.logging.log4j:log4j-api-kotlin:1.2.0")
+  runtimeOnly("com.mysql:mysql-connector-j")
+  testImplementation("org.springframework.boot:spring-boot-starter-test")
+  testImplementation("org.springframework.amqp:spring-rabbit-test")
 }
 
 tasks.withType<KotlinCompile> {
-	kotlinOptions {
-		freeCompilerArgs = listOf("-Xjsr305=strict")
-		jvmTarget = "1.8"
-	}
+  kotlinOptions {
+    freeCompilerArgs = listOf("-Xjsr305=strict")
+    jvmTarget = "1.8"
+  }
 }
 
 tasks.withType<Test> {
-	useJUnitPlatform()
+  useJUnitPlatform()
 }
--- cart-service/src/main/kotlin/com/makingdevs/cartservice/service/impl/ProductService.kt
+++ cart-service/src/main/kotlin/com/makingdevs/cartservice/service/impl/ProductService.kt
@@ -5,6 +5,7 @@ import com.makingdevs.cartservice.dto.PageDto
 import com.makingdevs.cartservice.repository.ProductRepository
 import com.makingdevs.cartservice.service.IProductService
 import java.util.*
+import org.apache.logging.log4j.kotlin.Logging
 import org.springframework.data.domain.Page
 import org.springframework.data.domain.Pageable
 import org.springframework.stereotype.Service
@@ -12,18 +13,23 @@ import org.springframework.stereotype.Service
 @Service
 class ProductService constructor(
   private val productRepository: ProductRepository
-) : IProductService {
+) : IProductService, Logging {
 
   override fun saveProduct(product: Product): Product {
+    logger.info("Save a new product")
     return productRepository.save(product)
   }
 
   override fun getAllProducts(pageable: Pageable): PageDto {
+    logger.info("Get all products")
+
     val pageProduct: Page<Product> = productRepository.findAll(pageable)
     return PageDto.toPageDto(pageProduct);
   }
 
   override fun deleteProduct(id: Long) {
+    logger.info("Delete product by id: $id")
+
     val product: Optional<Product> = productRepository.findById(id)
     if (product.isPresent) {
       productRepository.delete(product.get())
