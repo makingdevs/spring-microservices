b224bd50eb4aca25538b97ce9e66b19f4b83152e
Gilberto Reyes <gilbott@MacBook-Pro-de-Gilberto.local>
2022-12-09 13:50:54 -0600

    add ProductService implementation

---
+++ product-service/src/main/groovy/com/makingdevs/productservice/service/ProductServiceImpl.groovy
@@ -0,0 +1,34 @@
+package com.makingdevs.productservice.service
+
+import com.makingdevs.productservice.domain.Product
+import com.makingdevs.productservice.repository.ProductRepository
+import groovy.json.JsonBuilder
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.amqp.rabbit.core.RabbitTemplate
+import org.springframework.stereotype.Service
+
+@Service
+class ProductServiceImpl implements ProductService {
+
+  @Autowired
+  ProductRepository productRepository
+
+  @Autowired
+  RabbitTemplate rabbitTemplate
+
+  @Value('${application.rabbitmq.destination}')
+  String rabbitmqQueue
+
+  @Override
+  Product sendToCart(Long productId) {
+    Optional<Product> product = productRepository.findById(productId)
+    if (product.isPresent()) {
+      String jsonInString = new JsonBuilder(product.get()).toPrettyString()
+      rabbitTemplate.convertAndSend(rabbitmqQueue, jsonInString)
+      product.get()
+    }
+    null
+  }
+
+}
