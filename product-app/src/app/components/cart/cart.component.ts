ac099f66979c15269ca7734ba0a2b56c924cbbde
eduardo-rdguez <eduardoperaltardgz@gmail.com>
2022-12-09 18:08:04 -0600

    Add cart component

---
+++ product-app/src/app/components/cart/cart.component.ts
@@ -0,0 +1,30 @@
+import { Component, OnInit } from '@angular/core';
+import { Product } from 'src/app/models/product';
+import { PageReponse } from 'src/app/models/page-response';
+import { CartService } from 'src/app/services/cart.service';
+
+@Component({
+  selector: 'cart-component',
+  templateUrl: './cart.component.html',
+})
+export class CartComponent implements OnInit {
+  products: Product[] = [];
+  pages: number = 0;
+  total: number = 0;
+
+  constructor(private cartService: CartService) {}
+
+  ngOnInit(): void {
+    this.getProducts();
+  }
+
+  private getProducts(): void {
+    this.cartService
+      .getProducts()
+      .subscribe((response: PageReponse<Product>) => {
+        this.products = response.data;
+        this.pages = response.pages;
+        this.total = response.total;
+      });
+  }
+}
