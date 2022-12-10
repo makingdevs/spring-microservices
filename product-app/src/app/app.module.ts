75c01af8156dcdd3484dccc8f4c9f6524fa78748
eduardo-rdguez <eduardoperaltardgz@gmail.com>
2022-12-09 18:08:45 -0600

    Add cart and product components to app module

--- product-app/src/app/app.module.ts
+++ product-app/src/app/app.module.ts
@@ -1,18 +1,29 @@
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CartComponent } from './components/cart/cart.component';
+import { CartService } from './services/cart.service';
+import { ProductService } from './services/product.service';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CartComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [
+    CartService,
+    ProductService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
