a22d7126e150647ef1b4cf26df8b6edb2bca1738
Gilberto Reyes <gilbott@MacBook-Pro-de-Gilberto.local>
2022-12-13 12:50:01 -0600

    add eureka client configuration

--- product-service/src/main/groovy/com/makingdevs/productservice/ProductServiceApplication.groovy
+++ product-service/src/main/groovy/com/makingdevs/productservice/ProductServiceApplication.groovy
@@ -2,8 +2,10 @@ package com.makingdevs.productservice
 
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient
 
 @SpringBootApplication
+@EnableEurekaClient
 class ProductServiceApplication {
 
   static void main(String[] args) {
