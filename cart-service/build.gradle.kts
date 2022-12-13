cfece1bc6229aabeacb53a9f077742e8f5089fc4
Gilberto Reyes <gilbott@MacBook-Pro-de-Gilberto.local>
2022-12-13 12:48:54 -0600

    add eureka client configuration

--- cart-service/build.gradle.kts
+++ cart-service/build.gradle.kts
@@ -23,6 +23,7 @@ repositories {
 }
 
 dependencies {
+  implementation("org.springframework.cloud:spring-cloud-starter-netflix-eureka-client:3.1.4")
   implementation("org.springframework.boot:spring-boot-starter-amqp")
   implementation("org.springframework.boot:spring-boot-starter-data-jpa")
   implementation("org.springframework.boot:spring-boot-starter-web")
