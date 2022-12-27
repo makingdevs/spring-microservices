523d36e5b06d89d2f4c4da1c30fe665557da3d00
Gilberto Reyes <gilbott@MacBook-Pro-de-Gilberto.local>
2022-12-27 09:20:01 -0600

    add send to cart functionality

--- product-service-micronaut/src/main/groovy/com/makingdevs/productservice/service/ProductServiceImpl.groovy
+++ product-service-micronaut/src/main/groovy/com/makingdevs/productservice/service/ProductServiceImpl.groovy
@@ -1,14 +1,23 @@
 package com.makingdevs.productservice.service
 
 import com.makingdevs.productservice.domain.Product
+import com.makingdevs.productservice.repository.ProductRepository
 import javax.inject.Singleton
+import javax.inject.Inject
 
 @Singleton
 class ProductServiceImp implements ProductService{
 
+  @Inject
+  ProductProducerService productProducerService
+
+  @Inject
+  ProductRepository productRepository
+
   Product sendToCart(Long productId) {
-    println("sending product to queue...")
-    new Product()
+    Product product = productRepository.findById(productId).get()
+    productProducerService.send(product)
+    product
   }
 
 }
