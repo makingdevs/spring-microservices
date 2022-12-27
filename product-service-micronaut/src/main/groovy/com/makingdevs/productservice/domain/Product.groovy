486dcc6cf03548c967a70ed53b6fa813a04a1579
Gilberto Reyes <gilbott@MacBook-Pro-de-Gilberto.local>
2022-12-27 09:19:18 -0600

    remove @Column annotations

--- product-service-micronaut/src/main/groovy/com/makingdevs/productservice/domain/Product.groovy
+++ product-service-micronaut/src/main/groovy/com/makingdevs/productservice/domain/Product.groovy
@@ -1,11 +1,10 @@
 package com.makingdevs.productservice.domain
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.Table;
-import javax.persistence.GeneratedValue;
+import javax.persistence.Entity
+import javax.persistence.Table
+import javax.persistence.GeneratedValue
 import javax.persistence.GenerationType
-import javax.persistence.Id;
+import javax.persistence.Id
 
 @Entity
 @Table(name = "PRODUCTS")
@@ -14,11 +13,7 @@ class Product {
   @GeneratedValue(strategy = GenerationType.IDENTITY)
   @Id
   Long id
-
-  @Column
   String name
-
-  @Column
   Double price
 
 }
